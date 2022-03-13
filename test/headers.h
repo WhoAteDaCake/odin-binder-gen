52cb691e6e5213b58c08c6999e112040bdb480e0
WhoAteDaCake <doitsimplefy@gmail.com>
2022-03-13 20:58:29 +0000

    use headers

--- test/headers.h
+++ test/headers.h
@@ -1,10 +1,1 @@
-
-#include <wchar.h>
-
-// #include "python3.8/Python.h"
-int _Py_wreadlink(
-    const wchar_t *path,
-    wchar_t *buf,
-/* Number of characters of 'buf' buffer
-    including the trailing NUL character */
-    size_t buflen);
+#include "python3.8/Python.h"
